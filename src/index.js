da6d6b9692186ed68b1a2b0fc1d01c932973a928
sana0803 <sana0803@naver.com>
2022-03-07 18:06:55 +0900

    🎉 install redux, styled-components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import rootReducer from './reducers/index';
+import { createStore } from 'react-redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+// ROOT REDUCER
+import { dataReducer } from './dataReducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  dataReducer,
+});
+
+export default rootReducer;
