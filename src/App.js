0ac7b88040ddec7db4e97724ec1d1696cebbfd1e
yunsung-hodoopapa <vappet3477@gamail.com>
2022-03-07 23:54:32 +0900

    feat/skeleton-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Canvas from './pages/Canvas';
 import './App.css';
 import Results from './components/render/Results';
+
 const App = () => {
   return (
-    <div>
-      <Results />
-      {/* <Canvas /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/list' element={<Results />}/>
+        {/* <Canvas /> */}
+      </Routes>
+    </BrowserRouter>
   );
 };
 
