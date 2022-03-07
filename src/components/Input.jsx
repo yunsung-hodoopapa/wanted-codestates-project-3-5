8616d43292df0be0636d6adbe542f453c6619399
khw970421 <khw970421@kakao.com>
2022-03-07 18:22:48 +0900

    ✨ feat : url 구분하는 코드 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,24 +12,38 @@ const Input = () => {
     setApiRegionsData(regions);
   }, []);
 
+  function checkUrlForm(strUrl) {
+    /* eslint-disable */
+    let regex = new RegExp(
+      /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+    );
+    return regex.test(strUrl);
+  }
+
   const keyup = ({ code, target }) => {
     if (code === 'Enter') {
       //Todo : 2page와 3page간 enter 입력시 page이동 유무 차이
       const text = target.value;
-      const filterArr = apiProductData.filter(({ name, category_names }) => {
-        let isExist = false;
-        if (name.includes(text)) {
-          isExist = true;
-        } else if (searchKeyword[text]) {
-          category_names.map(category => {
-            searchKeyword[text].map(val => {
-              if (category.includes(val)) isExist = true;
+
+      // urlText or contentText
+      if (checkUrlForm(text)) {
+        console.log('url 실행');
+      } else {
+        const filterArr = apiProductData.filter(({ name, category_names }) => {
+          let isExist = false;
+          if (name.includes(text)) {
+            isExist = true;
+          } else if (searchKeyword[text]) {
+            category_names.map(category => {
+              searchKeyword[text].map(val => {
+                if (category.includes(val)) isExist = true;
+              });
             });
-          });
-        }
-        return isExist;
-      });
-      console.log(filterArr);
+          }
+          return isExist;
+        });
+        console.log(filterArr);
+      }
     }
   };
 
