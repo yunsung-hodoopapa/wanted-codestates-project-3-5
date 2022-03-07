18beee8b55b8c118860e4e77f35a17aff64ad178
NamgyungKim <namgyung.kim@gmail.com>
2022-03-07 21:19:40 +0900

    ✨ feat: name 입력 기능 추가, 스타일 적용

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -12,8 +12,20 @@ const Canvas = () => {
     const ctx = canvas.getContext('2d');
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    boxes.forEach(box => {
+    boxes.forEach((box, index) => {
+      ctx.strokeStyle = 'black';
+      ctx.strokeStyle = 'rgb(124,213,246)';
+      ctx.fillStyle = '#48aa3732';
+      if (index === boxes.length - 1 && isClick.current) {
+        ctx.strokeStyle = '#f34d98';
+        ctx.fillStyle = 'rgba(248,156,197,0.2)';
+      }
+      ctx.fillRect(box.startX, box.startY, box.width, box.height);
       ctx.strokeRect(box.startX, box.startY, box.width, box.height);
+
+      ctx.fillStyle = 'black';
+      ctx.font = '18px serif';
+      ctx.fillText(box.name, box.startX + 5, box.startY + 20);
     });
   }, [boxes]);
 
@@ -33,16 +45,18 @@ const Canvas = () => {
 
   const endPoint = ({ nativeEvent }) => {
     const { offsetX, offsetY } = nativeEvent;
-    setPoint(offsetX, offsetY);
+    const name = prompt('영역의 이름은 무엇인가요?');
+    setPoint(offsetX, offsetY, name);
     isClick.current = false;
   };
 
-  const setPoint = (offsetX, offsetY) => {
+  const setPoint = (offsetX, offsetY, name = '') => {
     const width = offsetX - boxes[len - 1].startX;
     const height = offsetY - boxes[len - 1].startY;
 
     boxes[len - 1].width = width;
     boxes[len - 1].height = height;
+    boxes[len - 1].name = name;
     setBoxes([...boxes]);
   };
 
@@ -57,6 +71,13 @@ const Canvas = () => {
         width="600"
         height="750"
       ></canvas>
+      <div>
+        <ul>
+          {boxes.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
     </Wrap>
   );
 };
@@ -65,6 +86,15 @@ const Wrap = styled.div`
   #canvas {
     background-image: url('img/fashion-unsplash.jpg');
     background-size: cover;
+    width: 600px;
+    height: 750px;
+  }
+  div {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    width: 200px;
+    background-color: #ffffff8f;
   }
 `;
 
