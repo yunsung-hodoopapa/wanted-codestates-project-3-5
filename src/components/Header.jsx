756e9589bdbaff4dd7b7818865ef77e309a1d2ee
jihyeon57 <ehwlgus22@gmail.com>
2022-03-08 14:45:55 +0900

    ✨ feat: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  h1 {
+    a {
+      display:block;
+      padding:20px;
+    }
+  }
+`
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <h1><Link to="/"><img src="../img/logo.png" alt="" /></Link></h1>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import Input from '../components/Input';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
+const SearchWrapper = styled.div`
+  width:1000px;
+  display:flex;
+  justify-content:space-around;
+  align-items: center;
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform: translate(-50%, -50%);
+  input {
+    width:700px;
+    box-sizing:border-box;
+    padding:20px;
+    border-radius:30px;
+    border:none;
+    box-shadow: 1px 5px 10px 3px #ebebeb;
+    font-size:1.1rem;
+  }
+`
 
 const Search = () => {
   return (
-    <div>
-      <Input />
-    </div>
+    <>
+      <Header />
+      <SearchWrapper>
+        <Input />
+        <button>검색</button>
+      </SearchWrapper>
+    </>
+
   );
 };
 
