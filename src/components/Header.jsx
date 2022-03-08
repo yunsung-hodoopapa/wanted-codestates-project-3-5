b61e4d917e77291e59200c91cd03d87ce9044c08
yunsung-hodoopapa <vappet3477@gamail.com>
2022-03-08 19:57:59 +0900

    Fix useLocation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const HeaderWrapper = styled.header`
-  position:fixed;
-  top:0;
-  width:100%;
+  position: fixed;
+  top: 0;
+  width: 100%;
   h1 {
     a {
-      display:block;
-      padding:20px;
+      display: block;
+      padding: 20px;
     }
   }
-`
+`;
 
 const Header = () => {
   return (
     <HeaderWrapper>
-      <h1><Link to="/"><img src="../img/logo.png" alt="" /></Link></h1>
+      <h1>
+        <Link to='/question1'>
+          <img src='../img/logo.png' alt='' />
+        </Link>
+      </h1>
     </HeaderWrapper>
   );
 };
